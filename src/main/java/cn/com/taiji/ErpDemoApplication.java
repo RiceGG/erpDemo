b3b9a981cb1a3904d59f7e3d9e8d758d7a1bf587
RiceGG <1948424125@qq.com>
2018-06-13 13:14:16 +0800

    new commit

--- src/main/java/cn/com/taiji/ErpDemoApplication.java
+++ src/main/java/cn/com/taiji/ErpDemoApplication.java
@@ -1,9 +1,15 @@
 package cn.com.taiji;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication  
+@EnableTransactionManagement  
+@ComponentScan("com.example.Controller")  
+@MapperScan("com.example.Dao")
 public class ErpDemoApplication {
 
 	public static void main(String[] args) {
